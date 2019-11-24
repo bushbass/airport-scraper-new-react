2419ceea6a81450f77823002c8ae7424e6c70980
Alex Nielsen <alex.nielsen@gmail.com>
2019-11-24 15:45:57 -0500

    removed unused components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Scraper from "./Scraper";
-import Nav from "./components/Nav";
 
 const App = () => {
   return <Scraper />;
