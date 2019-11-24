6478ce31c0ddbb679c194906bb8bb93b1af8620e
Alex Nielsen <alex.nielsen@gmail.com>
2019-11-24 15:44:41 -0500

    removed router and path stub component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Scraper from './Scraper';
-import PathTrains from './PathTrains';
-import Nav from './components/Nav';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Scraper from "./Scraper";
+import Nav from "./components/Nav";
 
 const App = () => {
-  return (
-    <Router>
-      <Nav />
-
-      <Route path='/scraper' exact component={Scraper} />
-      <Route path='/' exact component={PathTrains} />
-    </Router>
-  );
+  return <Scraper />;
 };
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
