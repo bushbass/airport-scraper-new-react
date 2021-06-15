0ff592289daf1a97dea9a5142959e67d5863be00
Alex Nielsen <alex@bigal.org>
2021-06-14 23:06:59 -0400

    added curly brackets to import Chart from chart.js in MyChart.js component

--- src/MyChart.js
+++ src/MyChart.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactChartkick, { BarChart } from "react-chartkick";
-import Chart from "chart.js";
+import React from 'react';
+import ReactChartkick, { BarChart } from 'react-chartkick';
+import { Chart } from 'chart.js';
 
 ReactChartkick.addAdapter(Chart);
 
 function makeArray(obj) {
   const newArray = [];
   let objKeys = Object.keys(obj);
-  objKeys.map(key => newArray.push([key, obj[key]]));
+  objKeys.map((key) => newArray.push([key, obj[key]]));
   return newArray;
 }
 
-const MyChart = props => {
+const MyChart = (props) => {
   return <BarChart data={makeArray(props.stateData)} />;
 };
 
